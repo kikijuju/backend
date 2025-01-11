54f6c4942bd252b351a886d7114fc1ada9f904f5
서민재 <hey_minj@naver.com>
2025-01-11 21:37:27 +0900

    환경 설정

---
+++ src/main/java/com/hanbat/tcar/TcarApplication.java
@@ -0,0 +1,13 @@
+package com.hanbat.tcar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TcarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TcarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hanbat/tcar/TcarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanbat.tcar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TcarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
