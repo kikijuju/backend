c4d02dd595eb74bd08160cb17d6ee2c182f798f5
서민재 <hey_minj@naver.com>
2025-01-20 21:03:39 +0900

    swagger

--- src/main/java/com/hanbat/tcar/user/UserController.java
+++ src/main/java/com/hanbat/tcar/user/UserController.java
@@ -1,5 +1,9 @@
 package com.hanbat.tcar.user;
 
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,11 +15,15 @@ import java.util.Optional;
 @RestController
 @RequestMapping("api/users/")
 @RequiredArgsConstructor
+@Tag(name="signup", description="회원가입")
 public class UserController {
     private final UserService userService;
 
 
     @PostMapping("/signup")
+//    @ApiResponse(responseCode = "200", description = "회원가입 성공")
+//            content = @Content(schema = @Schema(implementation = User.class)))
+//    @ApiResponse(responseCode = "400", description = "회원가입 실패")
     public ResponseEntity<UserSignupResponseDto> signUp(@RequestBody UserSignupRequestDto userSignupRequestDto){
         try{
             User user = userService.signUp(userSignupRequestDto);
